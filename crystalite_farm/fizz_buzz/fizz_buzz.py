0b23f947fca45aa78008a607598abef451b6130a
Luca Valentini <lv@mentalsmash.org>
2016-06-14 16:13:17 +0200

    added fizz buzz excercise

---
+++ crystalite_farm/fizz_buzz/fizz_buzz.py
@@ -0,0 +1,20 @@
+def fizz_buzz(number):
+    # replace this for solution
+    result = ""
+    if number % 3 == 0:
+        result += "Fizz"
+    if number % 5 == 0:
+        if result != "":
+            result += " "
+        result += "Buzz"
+    return str(number) if result == "" else result
+
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert fizz_buzz(15) == "Fizz Buzz", "15 is divisible by 3 and 5"
+    assert fizz_buzz(6) == "Fizz", "6 is divisible by 3"
+    assert fizz_buzz(5) == "Buzz", "5 is divisible by 5"
+    assert fizz_buzz(7) == "7", "7 is not divisible by 3 or 5"
+
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
